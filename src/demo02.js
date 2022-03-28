cea582431f9e805765a32f5f728e0fc562c9df24
qq449245884 <449245884@qq.com>
2022-03-28 23:52:03 +0800

    feat: 完成

--- src/demo02.js
+++ src/demo02.js
@@ -2,11 +2,17 @@
 const bucket = new Set()
 
 const data = { text: 'hello world'}
+// 保存当前被注册副作用函数
+let activeEffect
+function effect (fn) {
+  activeEffect = fn
+  fn()
+}
 
 const obj = new Proxy(data,  {
   // 拦截读取操作
   get (target, key) {
-    bucket.add(effect)
+    bucket.add(activeEffect)
     return target[key]
   },
   set (target, key, newVal) {
@@ -16,12 +22,11 @@ const obj = new Proxy(data,  {
   }
 })
 
-function effect() {
+effect(() => {
+  console.log('effect run')
   document.body.innerText = obj.text
-}
-
-effect()
+})
 
 setTimeout(() => {
-  obj.text = 'hello vue3'
+  obj.noExist = 'hello vue3'
 }, 3000)
