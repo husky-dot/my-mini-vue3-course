06b57494b49a778920673fbc9be506a5f888d098
qq449245884 <449245884@qq.com>
2022-03-29 23:42:40 +0800

    feat: 完成

--- src/demo02.js
+++ src/demo02.js
@@ -1,5 +1,5 @@
 
-const bucket = new Set()
+const bucket = new WeakMap()
 
 const data = { text: 'hello world'}
 // 保存当前被注册副作用函数
@@ -12,21 +12,36 @@ function effect (fn) {
 const obj = new Proxy(data,  {
   // 拦截读取操作
   get (target, key) {
-    bucket.add(activeEffect)
+    if (!activeEffect) return
+    let desMap = bucket.get(target)
+    if (!desMap) {
+      desMap = new Map()
+      bucket.set(target, desMap)
+    }
+    let deps = desMap.get(key)
+    if (!deps) {
+      deps = new Set()
+      desMap.set(key, deps)
+    }
+    deps.add(activeEffect)
     return target[key]
   },
   set (target, key, newVal) {
     target[key] = newVal
-    bucket.forEach(fn => fn())
+    const desMap = bucket.get(target)
+    if (!desMap) return
+    const effects = desMap.get(key)
+    effects && effects.forEach(fn => fn())
     return true
   }
 })
 
-effect(() => {
+effect(function effectFn() {
   console.log('effect run')
   document.body.innerText = obj.text
 })
 
 setTimeout(() => {
-  obj.noExist = 'hello vue3'
+  obj.noExits = 'hello vue3'
 }, 3000)
+
