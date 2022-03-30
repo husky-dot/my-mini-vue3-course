7299c3a139d4e353866518f212febba36f26e044
qq449245884 <449245884@qq.com>
2022-03-30 22:23:04 +0800

    feat: 完成

--- src/demo01.js
+++ src/demo01.js
@@ -1,12 +1,16 @@
-/**
- * 响应式数据的基本实现
- */
-const obj = {
-  text: 'vue2'
-}
-function effect() {
-  // 不执行
-  document.body.innerHTML = obj.text
-}
 
-obj.text = 'vue3'
+const map = new Map()
+const weakMap = new WeakMap()
+
+{
+
+  (function(){
+    const foo = { foo: 1}
+    const bar = { bar: 1 }
+  
+    map.set(foo, 1)
+    weakMap.set(bar, 2)
+  })()
+
+  console.log(map.keys())
+}
--- src/demo02.js
+++ src/demo02.js
@@ -4,34 +4,44 @@ const bucket = new WeakMap()
 const data = { text: 'hello world'}
 // 保存当前被注册副作用函数
 let activeEffect
+
 function effect (fn) {
   activeEffect = fn
   fn()
 }
 
+function track (target, key) {
+  if (!activeEffect) return
+  let desMap = bucket.get(target)
+  if (!desMap) {
+    desMap = new Map()
+    bucket.set(target, desMap)
+  }
+  let deps = desMap.get(key)
+  if (!deps) {
+    deps = new Set()
+    desMap.set(key, deps)
+  }
+  deps.add(activeEffect)
+}
+
+function trigger (target, key) {
+  const desMap = bucket.get(target)
+  if (!desMap) return
+  const effects = desMap.get(key)
+  effects && effects.forEach(fn => fn())
+}
 const obj = new Proxy(data,  {
   // 拦截读取操作
   get (target, key) {
-    if (!activeEffect) return
-    let desMap = bucket.get(target)
-    if (!desMap) {
-      desMap = new Map()
-      bucket.set(target, desMap)
-    }
-    let deps = desMap.get(key)
-    if (!deps) {
-      deps = new Set()
-      desMap.set(key, deps)
-    }
-    deps.add(activeEffect)
+    // 收集依赖
+    track(target, key)
     return target[key]
   },
   set (target, key, newVal) {
     target[key] = newVal
-    const desMap = bucket.get(target)
-    if (!desMap) return
-    const effects = desMap.get(key)
-    effects && effects.forEach(fn => fn())
+    // 触发依赖
+    trigger(target, key)
     return true
   }
 })
@@ -42,6 +52,6 @@ effect(function effectFn() {
 })
 
 setTimeout(() => {
-  obj.noExits = 'hello vue3'
+  obj.text = 'hello vue3'
 }, 3000)
 
